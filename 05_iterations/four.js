a9a7bd7c5668807de285c9fb9000be70e561f838
kk0707singh <kk0707singh@gmail.com>
2025-01-10 11:23:20 +0530

    js

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,37 +1,37 @@
-const MyObj  = {
-    js: 'javaScript',
-    cpp: 'C++',
-    rb: 'Rubby',
-    swift: 'Swift by apple'
-}
+// const MyObj  = {
+//     js: 'javaScript',
+//     cpp: 'C++',
+//     rb: 'Rubby',
+//     swift: 'Swift by apple'
+// }
 
-for (const key in MyObj) {
-    console.log(`${key} shortcut is for ${MyObj[key]}`);
-    
-}
-
-
-
-
-const progLang = ['js', 'rubby', 'python', 'cpp']
-for (const key in progLang) {
-    // console.log(key);
-    console.log(progLang[key]);
-    
-        
-}
-
-
-
-
-// const map = new Map()          //   MAP DECLARATION---------
-// map.set('IN', 'India')
-// map.set('USA', 'UnitedStatesOfAmerica')
-// map.set('RUS', 'Russia')
-// map.set('U.P.', 'UttarPradesh')
-
-// for (const key in map) {
-//     console.log(key);
-    
+// for (const key in MyObj) {
+//     console.log(`${key} shortcut is for ${MyObj[key]}`);
     
 // }
+
+
+
+
+// const progLang = ['js', 'rubby', 'python', 'cpp']
+// for (const key in progLang) {
+//     // console.log(key);
+//     console.log(progLang[key]);
+    
+        
+// }
+
+
+
+
+const map = new Map()          //   MAP DECLARATION---------
+map.set('IN', 'India')
+map.set('USA', 'UnitedStatesOfAmerica')
+map.set('RUS', 'Russia')
+map.set('U.P.', 'UttarPradesh')
+
+for (const key in map) {
+    console.log(key);
+    
+    
+}
